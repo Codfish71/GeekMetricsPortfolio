83d3de566dd31d38d37d321e180391af48d23241
Codfish71 <prathameshpendal71@gmail.com>
2023-12-14 18:02:17 +0530

    Routing for header and footer changed with global routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HomeComponent } from './core/components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { AboutUsComponent } from '../about-us/about-us.component';
+import { ServicesComponent } from '../services/services.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, AboutUsComponent],
+  imports: [HeaderComponent, FooterComponent, AboutUsComponent, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
